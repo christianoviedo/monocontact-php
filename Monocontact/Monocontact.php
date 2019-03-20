a3364a49b392d1c87b877bf0b582f05270004507
Christian Oviedo <coviedo@tallerdigital.cl>
2019-03-20 16:54:39 -0300

    First commit

---
+++ Monocontact/Monocontact.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Monocontact;
+
+class Monocontact {
+    public static function helloworld() {
+        return 'Hello World, Composer!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use Monocontact\Monocontact;
+
+echo Monocontact::helloworld();
